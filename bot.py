69aa90fd6d6160d78c1651675eb2a596cb756007
mortal096 <mortal0381@gmail.com>
2024-07-17 15:00:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ class Bot(Client):
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/weebs_support")
-        self.LOGGER(__name__).info(f"""deep""")
+        self.LOGGER(__name__).info(f"deep")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
